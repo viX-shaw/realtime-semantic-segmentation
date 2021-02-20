1d099029cdea412f343cb1f29323a9d7e6e98ffe
vivek <songokurocks551@gmail.com>
2021-02-20 12:33:04 +0530

    Change backround done!

--- src/index.js
+++ src/index.js
@@ -30,8 +30,13 @@ const modelPromise = load_model();
 class App extends React.Component {
   videoRef = React.createRef();
   canvasRef = React.createRef();
-
+  
   componentDidMount() {
+    let back_img = document.getElementById("myimg");
+
+    const back_img_pixels = tf.browser.fromPixels(back_img).toFloat();
+    // console.log("back_img_shape", back_img_pixels.shape)
+
     if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
       const webCamPromise = navigator.mediaDevices
         .getUserMedia({
@@ -51,7 +56,7 @@ class App extends React.Component {
         });
       Promise.all([modelPromise, webCamPromise])
         .then(values => {
-          this.detectFrame(this.videoRef.current, values[0]);
+          this.detectFrame(this.videoRef.current, values[0], back_img_pixels);
         })
         .catch(error => {
           console.error(error);
@@ -59,18 +64,19 @@ class App extends React.Component {
     }
   }
 
-  detectFrame = (video, model) => {
+  detectFrame = (video, model, back_img) => {
       tf.engine().startScope();
-      const predictions = model.predict(this.process_input(video));
-      this.renderPredictions(predictions);
+      const img = tf.browser.fromPixels(video).toFloat();
+      const predictions = model.predict(this.process_input(img));
+      this.renderPredictions(img, predictions, back_img);
       requestAnimationFrame(() => {
-        this.detectFrame(video, model);
+        this.detectFrame(video, model, back_img);
       });
       tf.engine().endScope();
   };
 
-  process_input(video_frame){
-    const img = tf.browser.fromPixels(video_frame).toFloat();
+  process_input(img){
+    // const img = tf.browser.fromPixels(video_frame).toFloat();
     const scale = tf.scalar(255.);
     const mean = tf.tensor3d([0.485, 0.456, 0.406], [1,1,3]);
     const std = tf.tensor3d([0.229, 0.224, 0.225], [1,1,3]);
@@ -78,37 +84,60 @@ class App extends React.Component {
     const batched = normalised.transpose([2,0,1]).expandDims();
     return batched;
   };
-    renderPredictions = async (predictions) => {
+  renderPredictions = async (img, predictions, back_img) => {
     const img_shape = [480, 480]
     const offset = 0;
     const segmPred = tf.image.resizeBilinear(predictions.transpose([0,2,3,1]),
                                               img_shape);
-    const segmMask = segmPred.argMax(3).reshape(img_shape);
-    const width = segmMask.shape.slice(0, 1);
-    const height = segmMask.shape.slice(1, 2);
-    const data = await segmMask.data();
-    const bytes = new Uint8ClampedArray(width * height * 4);
-    for (let i = 0; i < height * width; ++i) {
-      const partId = data[i];
-      const j = i * 4;
-      if (partId === -1) {
-          bytes[j + 0] = 255;
-          bytes[j + 1] = 255;
-          bytes[j + 2] = 255;
-          bytes[j + 3] = 255;
-      } else {
-          const color = pascalvoc[partId + offset];
+    let back_img_pixels = tf.browser.fromPixels(document.getElementById("myimg")).toFloat();
+    back_img_pixels = tf.image.resizeBilinear(back_img_pixels, img_shape)
+    img = tf.image.resizeBilinear(img, img_shape)
+    let segmMask = segmPred.argMax(3).reshape(img_shape);
+      //Class person id - 0
+    let personMask = tf.fill([480,480], 0)
+    segmMask = segmMask.equal(personMask)
 
-          if (!color) {
-              throw new Error(`No color could be found for part id ${partId}`);
-          }
-          bytes[j + 0] = color[0];
-          bytes[j + 1] = color[1];
-          bytes[j + 2] = color[2];
-          bytes[j + 3] = 255;
-      }
-    }
-    const out = new ImageData(bytes, width, height);
+    //Change Background    
+    // segmMask = segmMask.broadcastTo([3, 480, 480]).transpose([1,2,0])
+    // let final_img = back_img_pixels.where(segmMask, img)
+    // let alphaChannel = tf.fill([480,480,1], 255) 
+    // final_img = tf.concat([final_img, alphaChannel], 2)
+    // let img_buff = await final_img.data()
+
+    //Background blur
+    segmMask = segmMask.broadcastTo([1, 480, 480]).transpose([1,2,0])
+    // let alphaChannel = tf.fill([480,480,1], 190).where(segmMask, tf.fill([480,480,1], 255)) 
+    let alphaChannel = tf.randomUniform([480, 480, 1], 2.5, 5, 'float32', 42).exp().ceil().where(segmMask, tf.fill([480,480,1], 255)) 
+    let final_img = tf.concat([img, alphaChannel], 2)
+    let img_buff = await final_img.data()
+    
+    const bytes = Uint8ClampedArray.from(img_buff)
+
+    // const width = segmMask.shape.slice(0, 1);
+    // const height = segmMask.shape.slice(1, 2);
+    // const data = await segmMask.data();
+    // const bytes = new Uint8ClampedArray(width * height * 4);
+    // for (let i = 0; i < height * width; ++i) {
+    //   const partId = data[i];
+    //   const j = i * 4;
+    //   if (partId === -1) {
+    //       bytes[j + 0] = 255;
+    //       bytes[j + 1] = 255;
+    //       bytes[j + 2] = 255;
+    //       bytes[j + 3] = 255;
+    //   } else {
+    //       const color = pascalvoc[partId + offset];
+
+    //       if (!color) {
+    //           throw new Error(`No color could be found for part id ${partId}`);
+    //       }
+    //       bytes[j + 0] = color[0];
+    //       bytes[j + 1] = color[1];
+    //       bytes[j + 2] = color[2];
+    //       bytes[j + 3] = 255;
+    //   }
+    // }
+    const out = new ImageData(bytes, 480, 480);
     const ctx = this.canvasRef.current.getContext("2d");
     ctx.scale(1.5, 1.5);
     ctx.putImageData(out, 520, 60);
@@ -119,8 +148,9 @@ class App extends React.Component {
       <div>
         <h1>Real-Time Semantic Segmentation</h1>
         <h3>Refine Net</h3>
+        <img id="myimg" src={process.env.PUBLIC_URL + '/images/sky.jpg'} alt="sky" height="480" width="480"/>
         <video
-          style={{height: '600px', width: "480px"}}
+          style={{marginTop:480, height: '600px', width: "480px"}}
           className="size"
           autoPlay
           playsInline
